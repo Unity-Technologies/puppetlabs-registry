30449ef76057ca3063447c58848f19beed185cce
Dan Offord <dan.offord@multiplay.co.uk>
2015-07-10 16:37:09 +0100

    Add support HKEY_Users
    Add support for HKEY_USERS which was previously disabled

--- lib/puppet_x/puppetlabs/registry/provider_base.rb
+++ lib/puppet_x/puppetlabs/registry/provider_base.rb
@@ -128,6 +128,7 @@ module ProviderBase
         {
           :hkcr => Win32::Registry::HKEY_CLASSES_ROOT,
           :hklm => Win32::Registry::HKEY_LOCAL_MACHINE,
+          :hku  => Win32::Registry::HKEY_USERS,
         }
       end
     end
